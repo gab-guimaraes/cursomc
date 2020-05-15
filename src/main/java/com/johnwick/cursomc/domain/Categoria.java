1a071a07d21614bc91b686c0675f4d3c2cc4c216
Gabriel Guimaraes <dedito182@gmail.com>
2020-05-15 20:48:35 -0300

    criando categoria

--- src/main/java/com/johnwick/cursomc/domain/Categoria.java
+++ src/main/java/com/johnwick/cursomc/domain/Categoria.java
@@ -1,10 +1,7 @@
 package com.johnwick.cursomc.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -12,5 +9,37 @@ public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/johnwick/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/johnwick/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.johnwick.cursomc.resources;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.johnwick.cursomc.domain.Categoria;
+import com.johnwick.cursomc.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST ESTÁ FUNFANDO";
+    @Autowired
+    private CategoriaService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.buscar(id);
+        System.out.println("buscando" + id);
+        System.out.println(obj.toString());
+        return ResponseEntity.ok().body(obj);
+
     }
 
 }
