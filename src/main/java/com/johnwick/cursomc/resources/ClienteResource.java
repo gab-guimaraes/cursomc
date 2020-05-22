641d2c80a43d318d71d0625ed6c972c525fb6890
Gabriel Guimaraes <dedito182@gmail.com>
2020-05-22 12:46:32 -0300

    adicionando put categoria

--- src/main/java/com/johnwick/cursomc/resources/ClienteResource.java
+++ src/main/java/com/johnwick/cursomc/resources/ClienteResource.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.ClientEndpoint;
+
 @RestController
 @RequestMapping(value="/clientes")
 public class ClienteResource {
@@ -19,8 +21,8 @@ public class ClienteResource {
     private ClienteService service;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> find(@PathVariable Integer id) {
-        Cliente obj = service.buscar(id);
+    public ResponseEntity<Cliente> find(@PathVariable Integer id) {
+        Cliente obj = service.find(id);
         System.out.println("buscando" + id);
         System.out.println(obj.toString());
         return ResponseEntity.ok().body(obj);
